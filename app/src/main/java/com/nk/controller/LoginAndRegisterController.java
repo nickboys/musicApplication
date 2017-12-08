5675f59b6a9a1a9c48c5119c76d0517f68e24366
JianHuangsh <JianHuangsh@quarkfinance.com>
2017-12-08 19:21:55 +0800

    add JPA

--- app/src/main/java/com/nk/controller/LoginAndRegisterController.java
+++ app/src/main/java/com/nk/controller/LoginAndRegisterController.java
@@ -1,5 +1,9 @@
 package com.nk.controller;
 
+import com.nk.model.entity.User;
+import com.nk.service.UserService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoginAndRegisterController {
 
-
+  @Autowired
+  UserService userService;
   @RequestMapping(value = "/hello", method = RequestMethod.GET)
   public String hello() throws Exception {
     //throw  new MyException("error");
-    return "hello world";
+    List<User> users=userService.findUser();
+
+    System.out.print(users.size());
+    return "hello world"+users.get(0).getUsername();
   }
 }
--- app/src/main/java/com/nk/service/UserService.java
+++ app/src/main/java/com/nk/service/UserService.java
@@ -1,19 +1,22 @@
-//package com.nk.service;
-//
-//import com.nk.repository.UserRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-///**
-// * Created by JianHuangsh on 2017/12/8.
-// */
-//@Service
-//public class UserService {
-//
-//  @Autowired
-//  private UserRepository userRepository;
-//
-//  public void findUser() {
-//    userRepository.findAll();
-//  }
-//}
+package com.nk.service;
+
+import com.nk.model.entity.User;
+import com.nk.repository.UserRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by JianHuangsh on 2017/12/8.
+ */
+@Service
+public class UserService {
+
+  @Autowired
+  private UserRepository userRepository;
+
+  public List<User> findUser() {
+    List<User> users= userRepository.findAll();
+    return  users;
+  }
+}
