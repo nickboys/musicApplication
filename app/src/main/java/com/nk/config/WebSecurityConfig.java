f2e3cbbbb83673a22101382212c2b59dade9ef31
JianHuangsh <JianHuangsh@quarkfinance.com>
2017-12-08 19:24:52 +0800

    add

--- app/src/main/java/com/nk/config/WebSecurityConfig.java
+++ app/src/main/java/com/nk/config/WebSecurityConfig.java
@@ -1,13 +1,17 @@
 package com.nk.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * Created by JianHuangsh on 2017/12/8.
  */
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
